d60bc90629f0ddd6eeb6c48762683dc09a5da7e0
Muaz Ali <muazali2003@gmail.com>
2025-08-29 10:33:39 -0700

    ScrollToTop added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer'; 
 import Home from './pages/Home';
@@ -7,10 +8,25 @@ import Products from './pages/Products';
 import Contact from './pages/Contact';
 import OurClients from './pages/OurClients';
 
+// ScrollToTop with smooth effect
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth" // 👈 smooth scroll
+    });
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Navbar />
       
       <Routes>
