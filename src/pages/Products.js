d4df6b34d336c13f97bbc65244cea67723141b23
Muaz Ali <muazali2003@gmail.com>
2025-08-29 04:52:54 -0700

    Product changes

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -25,72 +25,72 @@ const ProductSection = () => {
 
   const products = [
     {
-      title: "Front Marker Lamp",
+      title: "FRONT MARKER LAMP",
       img: "/images/lights0.png",
       desc: "Front marker lights are typically mounted below or to the side of the vehicle's normal headlights. They help make the vehicle visible to oncoming drivers or indicate a vehicle is following.",
     },
     {
-      title: "Rear Blackout Lights",
+      title: "REAR BLACKOUT LIGHTS",
       img: "/images/lights.jpg",
       desc: "Rear marker lights or blackout taillights are typically housed in the same unit as the vehicles taillights, and are the main tool in keeping the proper following distance in nighttime convoy driving.",
     },
     {
-      title: "Convoy Lamp",
+      title: "CONVOY LAMP",
       img: "/images/lights2.jpg",
       desc: "Convoy Lamps are mounted on the driver's side of the vehicle, typically on the fender. They give off a diffused beam of white light that replaces regular headlights when driving under blackout conditions.",
     },
     {
-      title: "Map Reading Light",
+      title: "MAP READING LIGHT",
       img: "/images/lights3.png",
       desc: "Map reading light is installed on the passenger side dashboard or inside the glove compartment. It is used for reading maps with sufficient light.",
     },
     {
-      title: "Blackout Tail Light Assembly",
+      title: "BLACKOUT TAIL LIGHT ASSEMBLY",
       img: "/images/lights8.png",
       desc: "This is a military-style vehicle tail light assembly, commonly used in heavy-duty vehicles, military trucks, or off-road applications.",
     },
     {
-      title: "Submachine Gun Holders",
+      title: "SUBMACHINE GUN HOLDERS",
       img: "/images/lights4.png",
       desc: "As the name suggests, SMG Rack is used to hold guns while the vehicle is in movement. It comprises two parts: Rack Top & Rack Bottom.",
     },
     {
-      title: "12 Pin Socket Assembly",
+      title: "12 PIN SOCKET ASSEMBLY",
       img: "/images/lights5.png",
       desc: "An electrical connector between vehicle and trailer, supplying automotive lighting and controlling the trailer.",
     },
     {
-      title: "Jumper Cable (16mm2)",
+      title: "JUMPER CABLE (16mm2)",
       img: "/images/lights6.png",
       desc: "A jumper cable with thick electrical cable size 16mm2 and copper clamps. Used to head start a car or another vehicle.",
     },
     {
-      title: "12 Pin / Receptacle Cable (SPIRAL TYPE)",
+      title: "12 PIN / RECEPTACLE CABLE (SPIRAL TYPE)",
       img: "/images/lights7.png",
       desc: "A spiral cable with a receptacle at one end and 12 pin at the other end, used to connect the trailer with the vehicle.",
     },
     {
-      title: "12 Pin / Receptacle Cable (STRAIGHT TYPE)",
+      title: "12 Pin / RECEPTACLE CABLE (STRAIGHT TYPE)",
       img: "/images/lights9.png",
       desc: "A straight cable with a receptacle at one end and 12 pin at the other end, used to connect the trailer with the vehicle.",
     },
     {
-      title: "Jumper Cable (35mm2)",
+      title: "JUMPER CABLE (35mm2)",
       img: "/images/lights11.png",
       desc: "A 3 or 5 meter jumper cable, size 35mm2, with clamps at one end and male switch at the other, for heavy-duty vehicles.",
     },
     {
-      title: "Jumper Cable (50mm2)",
+      title: "JUMPER CABLE (50mm2)",
       img: "/images/lights10.png",
       desc: "A jumper cable size 50mm2 with clamps at one end and male switch at the other, used for heavy duty vehicles.",
     },
     {
-      title: "Jumper Socket",
+      title: "JUMPER SOCKET",
       img: "/images/lights12.png",
       desc: "A jumper lead socket attached to the vehicle’s battery, used to jump start the vehicle.",
     },
     {
-      title: "Inspection Lamp",
+      title: "INSEPTION LAMP",
       img: "/images/lights14.png",
       desc: "A long wired lamp with battery clamps at each end. Can be directly connected to a battery for working in dark.",
     },
